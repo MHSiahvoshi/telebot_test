cc63c7a0eb3eedc7d9f6f3bb2f7ca92262fbc6fc
MHS1105 <mohammadhosein11051377@gmail.com>
2023-10-10 12:52:59 -0700

    use tkinter

---
+++ main.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+window = tk.Tk()
+greeting = tk.Label(text="Hello, Tkinter", background='lightblue', foreground='azure1', height=10, width=10)
+greeting.pack()
+test = tk.Button(text='fuck me')
+test.pack()
+window.mainloop()
