a5222b3a8e737855d4e2b7974d022dac732006cf
MHS1105 <mohammadhosein11051377@gmail.com>
2023-10-13 08:45:06 -0700

    changed button

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 window = tk.Tk()
-greeting = tk.Label(text="Hello, Tkinter", background='lightblue', foreground='azure1', height=10, width=10)
+greeting = tk.Label(text="Hello, Tkinter", background='green', foreground='azure1', height=10, width=10)
 greeting.pack()
 test = tk.Button(text='click me')
 test.pack()
