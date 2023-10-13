9b68e8c4cda4901e9a1ff30cd9ae24aa163c52e4
MHS1105 <mohammadhosein11051377@gmail.com>
2023-10-13 01:44:38 -0700

    change main

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import tkinter as tk
 window = tk.Tk()
 greeting = tk.Label(text="Hello, Tkinter", background='lightblue', foreground='azure1', height=10, width=10)
 greeting.pack()
-test = tk.Button(text='fuck me')
+test = tk.Button(text='click me')
 test.pack()
 window.mainloop()
-print("hello")
-print("how")
+
