f5ce8bb016a35ee065a1a60b98f28db923a08b7b
MHS1105 <mohammadhosein11051377@gmail.com>
2023-10-27 11:16:43 -0700

    we use reply to for help

--- main.py
+++ main.py
@@ -7,5 +7,10 @@ def send_welcome(message):
     bot.send_message(message.chat.id, 'hi\nwelcome')
 
 
+@bot.message_handler(commands=['help'])
+def send_help(message):
+    bot.reply_to(message, "What can i do for you?")
+
+
 bot.infinity_polling()
 
