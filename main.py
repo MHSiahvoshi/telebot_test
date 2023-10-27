80ad32c561ae79844227fa2be0f553264f9d672e
MHS1105 <mohammadhosein11051377@gmail.com>
2023-10-27 13:29:19 -0700

    until now

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
 import telebot
 bot = telebot.TeleBot("6732287229:AAEHcZFArgAgw_XKx8ZJHChzGRcSpZU8apo")
+first_button = telebot.types.InlineKeyboardButton("Downloader_bot", url="https://t.me/Danlodrbot")
+markup = telebot.types.InlineKeyboardMarkup()
+markup.add(first_button)
+key_button = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+key_button.add("TwiteNudes", "Ssskade", "baad_pussy")
+# get_contact = telebot.types.KeyboardButton("black")
 
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.send_message(message.chat.id, 'hi\nwelcome')
+    bot.send_message(message.chat.id, 'hi\nwelcome', reply_markup=markup)
 
 
 @bot.message_handler(commands=['help'])
 def send_help(message):
-    bot.reply_to(message, "What can i do for you?")
+    bot.send_message(message.chat.id, 'what can i do for you baby?', reply_markup=key_button)
+
+
+@bot.message_handler()
+def keyboard_help(message):
+    if message.text == "TwiteNudes":
+        bot.send_message(message.chat.id, "@TwiteNuds_Bot")
+    elif message.text == "Ssskade":
+        bot.send_message(message.chat.id, "@Sskkadebot")
+    elif message.text == "baad_pussy":
+        bot.send_message(message.chat.id, "@baad_pussy_Bot")
 
 
 bot.infinity_polling()
